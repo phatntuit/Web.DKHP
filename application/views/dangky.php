c0e0921282ce7d27c46f8a6b88aeaaead16dcc50
nguyennghia2909 <nghianguyen2909@gmail.com>
2016-05-05 11:09:19 +0700

    sửa
    sửa file header để xét title

---
+++ application/views/dangky.php
@@ -0,0 +1,11 @@
+	<div class="row"></div>
+	<section id="content">
+		<div class="row" id="result">
+			Thông tin đăng ký
+		</div>
+	</section>
+	<?php $this->load->view('template/about.php'); ?>
+	<?php $this->load->view('template/footer.php'); ?>
+</body>
+
+</html>
