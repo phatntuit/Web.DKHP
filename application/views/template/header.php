d3a0b172305f4415c7ca9170443c0bad08e3c37a
phatntuit <phatpropk@gmail.com>
2016-05-06 21:46:54 +0700

    update css of bootstrap

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,39 +10,61 @@
   <title><?php echo $page_title;?></title>
 
   <!-- Bootstrap Core CSS -->
-  <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url();?>assets/css/bootstrap.css" rel="stylesheet">
 
   <!-- Fonts -->
-  <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url();?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
   <link href="<?php echo base_url();?>assets/css/animate.css" rel="stylesheet" />
   <!-- Squad theme CSS -->
   <link href="<?php echo base_url();?>assets/css/style.css" rel="stylesheet">
   <link href="<?php echo base_url();?>assets/color/default.css" rel="stylesheet">
+  <!-- Core JavaScript Files -->
+  <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url();?>assets/js/jquery.easing.min.js"></script>  
+  <script src="<?php echo base_url();?>assets/js/jquery.scrollTo.js"></script>
+  <script src="<?php echo base_url();?>assets/js/wow.min.js"></script>
+  <!-- Custom Theme JavaScript -->
+  <script src="<?php echo base_url();?>assets/js/custom.js"></script>
   <script type="text/javascript">
     // Ajax post
-    $(document).ready(function() 
+    // $(document).ready(function() 
+    // {
+    //   $("#login").click(function(e) 
+    //   {
+    //     e.preventDefault();
+    //     var user_name = $("#username").val();
+    //     var password = $("#pwd").val();
+    //     jQuery.ajax(
+    //     {
+    //       type: "POST",
+    //       url: "<?php echo site_url('Home/Login'); ?>",
+    //       dataType: 'json',
+    //       data: {name: user_name, pwd: password},
+    //       success: function(res)
+    //       {
+    //         if (res)
+    //         {
+    //           alert('sucess');
+    //         }
+    //       }
+    //     });
+    //   });
+    // });
+    function login()
     {
-      $("#login").click(function(e) 
+      var url="<?php echo site_url(''); ?>";
+      $.ajax(
       {
-        e.preventDefault();
-        var user_name = $("#username").val();
-        var password = $("#pwd").val();
-        jQuery.ajax(
-        {
-          type: "POST",
-          url: "<?php echo base_url(); ?>Home/Login",
-          dataType: 'json',
-          data: {name: user_name, pwd: password},
-          success: function(res)
-          {
-            if (res)
-            {
-              alert('sucess');
-            }
-          }
-        });
+        url:url,
+        type:'POST',
+        data:$('#form').serialize(),
+        dataType:'JSON',
+        sucess:function(data){
+          $('#myModal').modal('hide');
+        }
       });
-    });
+    }
   </script>
 </head>
 <body id="page-top" data-spy="scroll" data-target=".navbar-custom">
@@ -83,48 +105,48 @@
         <li><a href="#">Liên hệ</a></li>
         <li><a href="#about">Về chúng tôi</a></li>
         <?php 
-          if(isset($username))
-          {
-        ?>
-            <li id="user"><a href="#"><?php echo $username; ?></a></li>
-        <?php
-          }
-          else
-          {
+        if(isset($username))
+        {
           ?>
-           <li id="loginform"><a>Đăng nhập</a></li>
+          <li id="user"><a href="#"><?php echo $username; ?></a></li>
+          <?php
+        }
+        else
+        {
+          ?>
+          <li id="loginform"><a href="#">Đăng nhập</a></li>
           <?php } ?>
-      </ul>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
     </div>
-    <!-- /.navbar-collapse -->
-  </div>
-  <!-- /.container -->
-</nav>
-<!-- login form -->
-<div class="modal fade" id="myModal" role="dialog">
-  <div class="modal-dialog">
+    <!-- /.container -->
+  </nav>
+  <!-- login form -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header" style="padding:35px 50px;">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4><span class="glyphicon glyphicon-log-in"  style="font-size:1.3em;"></span> Login</h4>
-      </div>
-      <div class="modal-body" style="padding:40px 50px;">
-        <form role="form" method="post">
-          <div class="form-group">
-            <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
-            <input type="text" class="form-control" id="username" name="username" placeholder="User name">
-          </div>
-          <div class="form-group">
-            <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Passwords</label>
-            <input type="password" class="form-control" id="psw" name="pwd" placeholder="Passwords">
-          </div>
-          <button type="button" class="btn btn-success btn-block" id="login"><span class="glyphicon glyphicon-off"></span>Login</button>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-danger btn-default pull-right" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header" style="padding:35px 50px;">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4><span class="glyphicon glyphicon-log-in"  style="font-size:1.3em;"></span> Login</h4>
+        </div>
+        <div class="modal-body" style="padding:40px 50px;">
+          <form role="form" method="post" action="#" id="form">
+            <div class="form-group">
+              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
+              <input type="text" class="form-control" id="username" name="username" placeholder="User name">
+            </div>
+            <div class="form-group">
+              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Passwords</label>
+              <input type="password" class="form-control" id="psw" name="pwd" placeholder="Passwords">
+            </div>
+            <button type="button" class="btn btn-success btn-block" id="login" onclick="login()"><span class="glyphicon glyphicon-off"></span>Login</button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-danger btn-default pull-right" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
           <!-- <p>Not a member? <a href="#">Sign Up</a></p>
           <p>Forgot <a href="#">Password?</a></p> -->
         </div>
