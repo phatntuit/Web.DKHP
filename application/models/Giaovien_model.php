c22df05b3979944b6ec73cb1f3d49a4c4a1bec13
quyenanh928 <vonganhquyen@gmail.com>
2016-05-08 18:06:41 +0700

    giáo viên

--- application/models/Giaovien_model.php
+++ application/models/Giaovien_model.php
@@ -1,11 +1,18 @@
 <?php
-class Hocphi_model extends CI_model
+class Giaovien_model extends CI_model
 {
 	public function show()
 	{
-		$query = $this->db->get('sinhvien');
+		$query = $this->db->get('giaovien');
 		$query_result = $query->result_object();
 		return $query_result;
+	}
+	public function get_by_id($id)
+	{
+		$this->db->from($this->table);
+		$this->db->where('Magiaovien',$id);
+		$query = $this->db->get();
+		return $query->row();
 	}
 }
 ?>
