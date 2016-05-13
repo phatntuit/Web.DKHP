e9600009e428b518814822075a3622bac57baca5
phatntuit <phatpropk@gmail.com>
2016-05-13 22:16:46 +0700

    test ajax post

--- application/views/sinhvien_view.php
+++ application/views/sinhvien_view.php
@@ -52,19 +52,15 @@ $this->load->view('template/header');?>
 						<h4 class="modal-title">Thêm sinh viên</h4>
 					</div>
 					<div class="modal-body">
-						<form action="#" method="POST" role="form">
-							<legend>Form title</legend>
-						
 							<div class="form-group">
-								<label for="">label</label>
-								<input type="text" class="form-control" id="" placeholder="Input field">
+								<label>Input :</label>
+								<input type="text" class="form-control" id="test" placeholder="Input field">
 							</div>
-							<button type="submit" class="btn btn-primary">Submit</button>
 						</form>
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="submit" class="btn btn-primary">Save</button>
 					</div>
 				</div>
 			</div>
