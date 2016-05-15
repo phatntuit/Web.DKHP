4b8bb66441ff7437030b5a48b519a0bd89405154
phatntuit <phatpropk@gmail.com>
2016-05-15 22:08:57 +0700

    taoma hp

--- application/controllers/Hocphan.php
+++ application/controllers/Hocphan.php
@@ -24,6 +24,11 @@ class Hocphan extends CI_Controller
         $this->load->view('template/header',$this->data);
 		$this->load->view('hocphan/thongtinhocphan');   
 	}
+	public function Taoma()
+	{
+		$re=$this->Hocphan_model->Taomath("IS201.H01");
+		echo $re;
+	}
 	public function get_new_data(){
 		$hocphan=$this->Hocphan_model->gethocphan();
 		$tb='';
