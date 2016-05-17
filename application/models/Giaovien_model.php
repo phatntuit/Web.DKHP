65cc472a1bf421b4e5279d3b2e00b6cf3174ebd1
quyenanh928 <vonganhquyen@gmail.com>
2016-05-17 20:48:43 +0700

    gv

--- application/models/Giaovien_model.php
+++ application/models/Giaovien_model.php
@@ -10,12 +10,6 @@ class Giaovien_model extends CI_model
 	public function update($where, $data)
 	{
 		$this->db->update('giaovien', $data, $where);
-		//return $this->db->affected_rows();
-		//$ma = $this->input->get('Magiaovien');
-		//echo $ma;
-		//$this->db->where('Magiaovien',$ma);
-		//$this->db->where('Magiaovien',$ma);
-		//$this->db->update('giaovien',$data);
 	}
 
 	public function add($data)
@@ -27,14 +21,6 @@ class Giaovien_model extends CI_model
 	{
 		$this->db->where('Magiaovien', $id);
 		$this->db->delete('giaovien');
-	}
-	function get_datatables()
-	{
-		$this->_get_datatables_query();
-		if($_POST['length'] != -1)
-		$this->db->limit($_POST['length'], $_POST['start']);
-		$query = $this->db->get();
-		return $query->result();
 	}
 	public function get_by_id($id)
 	{
@@ -49,57 +35,6 @@ class Giaovien_model extends CI_model
 		$result = $giaovien->result_object();
 		mysqli_next_result( $this->db->conn_id );
 		return $result;
-	}
-	
-	private function _get_datatables_query()
-	{
-		
-		$this->db->from($this->table);
-
-		$i = 0;
-	
-		foreach ($this->column_search as $item) // loop column 
-		{
-			if($_POST['search']['value']) // if datatable send POST for search
-			{
-				
-				if($i===0) // first loop
-				{
-					$this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
-					$this->db->like($item, $_POST['search']['value']);
-				}
-				else
-				{
-					$this->db->or_like($item, $_POST['search']['value']);
-				}
-
-				if(count($this->column_search) - 1 == $i) //last loop
-					$this->db->group_end(); //close bracket
-			}
-			$i++;
-		}
-		
-		if(isset($_POST['order'])) // here order processing
-		{
-			$this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-		} 
-		else if(isset($this->order))
-		{
-			$order = $this->order;
-			$this->db->order_by(key($order), $order[key($order)]);
-		}
-	}
-	function count_filtered()
-	{
-		$this->_get_datatables_query();
-		$query = $this->db->get();
-		return $query->num_rows();
-	}
-
-	public function count_all()
-	{
-		$this->db->from($this->table);
-		return $this->db->count_all_results();
 	}
 }
 ?>
